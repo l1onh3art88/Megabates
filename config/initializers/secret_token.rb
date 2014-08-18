4002ec78f2cee3e50ffe55c91f556b3a2c2283c5
vagrant <vagrant@rails-dev-box>
2014-08-18 15:35:27 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Megabates::Application.config.secret_key_base = '27f3e62ef60b86db04af5ce3286b5b98ae9a0e2fe2d5f88e13d65ab636b454577d8a5a338d440c983d0d2fec19eabbd811a0bef347947368b67aac98c70518c2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Megabates::Application.config.session_store :cookie_store, key: '_megabates_session'
